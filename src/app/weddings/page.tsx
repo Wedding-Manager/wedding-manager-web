d80f8cccc828a97fbe5f063ab31437a0d854a8e5
shekhar kummari <shekhar.kummari@wizfreight.com>
2024-01-07 03:35:04 +0530

    fixing build issues

--- src/app/weddings/page.tsx
+++ src/app/weddings/page.tsx
@@ -1,36 +1,7 @@
-import CustomInput from "@/components/input";
-import { weddingSubmitHandler } from "@/stores/weddings";
-import { WeddingFormData } from "@/types/weddings";
-import React, { Ref } from "react";
-import { useForm } from "react-hook-form";
+import React from "react";
 
 function Weddings() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<WeddingFormData>();
-  return (
-    <div>
-      <div>
-        <form onSubmit={handleSubmit(weddingSubmitHandler)}>
-          <CustomInput
-            isInvalid={!!errors?.title}
-            errorMessage={errors?.title?.message}
-            label={"Wedding Title"}
-            {...register("title", {
-              required: {
-                value: true,
-                message: "Name is required",
-              },
-            })}
-            placeholder="enter name"
-            className="border-solid border-2 border-indigo-600"
-          />
-        </form>
-      </div>
-    </div>
-  );
+  return <div>Under Progress</div>;
 }
 
 export default Weddings;
